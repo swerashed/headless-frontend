10a4164c3f8d1f3207b59ea823862b934f36a6d8
Abdul Muhaimin Toha <toha@notionhive.com>
2024-11-06 01:31:00 +0600

    deployment/initial problem solved v.2

--- app/page.js
+++ app/page.js
@@ -5,9 +5,7 @@ import CoverageArea from "./components/coverageArea/CoverageArea";
 import Footer from "./components/footer/Footer";
 import HeroSlider from "./components/heroSlider/HeroSlider";
 import HowWeHelp from "./components/howWeHelp/HowWeHelp";
-import OurInvestors from "./components/ourInvestors/OurInvestors";
 import OurPartners from "./components/ourPartners/OurPartners";
-import RecentUpdates from "./components/recentUpdates/RecentUpdates";
 
 export default function Home() {
   return (
@@ -18,8 +16,6 @@ export default function Home() {
       <HowWeHelp />
       <CoverageArea />
       <OurPartners />
-      <OurInvestors />
-      <RecentUpdates />
       <ContactUsBanner />
       <Footer />
     </>

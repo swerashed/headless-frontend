a71f7334c1b60b20b20653b49a6fb92f79260b77
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-11-10 18:25:29 +0600

    hero gradient done

--- components/heroBanners/HeroBannerSecondary.jsx
+++ components/heroBanners/HeroBannerSecondary.jsx
@@ -11,7 +11,7 @@ function HeroBannerSecondary() {
         width={1331}
         height={438}
       />
-      <div className="absolute inset-0 flex flex-col justify-end">
+      <div className="absolute inset-0 flex flex-col justify-end bg-dark/10 bg-gradient-to-t from-dark to-dark/0">
         <div className="container flex flex-col gap-[10px] pb-[30px] md:pb-[60px]">
           <h3 className="font-inter text-sm font-medium uppercase text-white md:text-base">
             About us

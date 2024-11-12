a688638e137918cf877231e5051a992623c152fc
Rashed <tools.rashed@gmail.com>
2024-11-12 12:09:12 +0600

    Updated people card

--- components/peopleCards/PeopleCard.jsx
+++ components/peopleCards/PeopleCard.jsx
@@ -23,7 +23,6 @@ function PeopleCard({ people }) {
               alt={people.name}
               width={400}
               height={439}
-              style={{ width: "auto", height: "auto" }}
               className="aspect-[11/12] w-full object-cover transition-all duration-300 group-hover:scale-105 md:aspect-[41/45]"
             />
           </div>

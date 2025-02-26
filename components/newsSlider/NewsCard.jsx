b5e8ef5e72a2614b62634cadd61d91d17cba9a34
Rashed <rashed@notionhive.com>
2025-02-26 12:34:26 +0600

    Added details page link to card

--- components/newsSlider/NewsCard.jsx
+++ components/newsSlider/NewsCard.jsx
@@ -1,5 +1,6 @@
 import { getPost } from "@/graphql/blogs/GET_POST";
 import Image from "next/image";
+import Link from "next/link";
 
 function NewsCard({blog}) {
   const {title, date, content, featuredImage, slug, uri} = blog;
@@ -11,7 +12,7 @@ function NewsCard({blog}) {
   // Format the date
   const options = { year: "numeric", month: "long", day: "numeric" };
   const formattedDate = new Intl.DateTimeFormat("en-US", options).format(dateObject);
-
+console.log(slug)
 
   return (
     <div
@@ -19,6 +20,7 @@ function NewsCard({blog}) {
       className="group flex cursor-pointer flex-col overflow-hidden rounded-md"
     >
       <div className="mb-5 w-full overflow-hidden rounded-md">
+        <Link href={`/news/${slug}`}>
         <Image
           src={featuredImage?.node?.sourceUrl}
           width={410}
@@ -26,13 +28,17 @@ function NewsCard({blog}) {
           alt={title}
           className="aspect-[330/175] w-full object-cover transition-all duration-1000 group-hover:scale-110 md:aspect-[410/240]"
         />
+        </Link>
       </div>
       <p className="mb-[10px] font-inter text-[14px] font-normal uppercase leading-[22px] text-dark/80">
         {formattedDate}
       </p>
+
+      <Link href={`/news/${slug}`}>
       <h4 className="line-clamp-2 font-onest text-[20px] font-medium leading-[30px] text-dark md:text-[24px] md:leading-[34px]">
         {title}
       </h4>
+      </Link>
     </div>
   );
 }

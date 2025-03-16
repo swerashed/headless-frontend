88867f512cb53f791c79809f978346170cf6ac4b
Abdul Muhaimin Toha <toha@notionhive.com>
2025-03-16 09:25:59 +0600

    qa done

--- components/newsHero/FeatureNewsLargeCard.jsx
+++ components/newsHero/FeatureNewsLargeCard.jsx
@@ -3,18 +3,22 @@ import Link from "next/link";
 import React from "react";
 
 function FeatureNewsLargeCard({ news }) {
-  const { title, featuredImage, date } = news
+  const { title, featuredImage, date } = news;
   // Convert the string to a Date object
   const dateObject = new Date(date);
 
   // Format the date
   const options = { year: "numeric", month: "long", day: "numeric" };
-  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(dateObject);
+  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+    dateObject,
+  );
   return (
-    <div className="group relative col-span-3 flex h-full overflow-hidden rounded-[20px] xl:col-span-4">
+    <Link
+      href={`/news/${news?.slug}`}
+      className="group relative col-span-3 flex h-full overflow-hidden rounded-[20px] xl:col-span-4"
+    >
       <div className="overflow-hidden">
         <Link href={`/news/${news?.slug}`}>
-
           <Image
             src={featuredImage?.node?.sourceUrl || "/aks-thumbnail.png"}
             width={850}
@@ -22,20 +26,21 @@ function FeatureNewsLargeCard({ news }) {
             alt={title}
             className="absolute inset-0 h-full w-full object-cover transition-all duration-700 group-hover:scale-105"
           />
-
         </Link>
       </div>
       <div className="absolute inset-0 flex items-end bg-gradient-to-t from-[#000511CC] to-[#00206800]">
         <div className="flex flex-col p-10 font-inter text-white">
-          <p className="mb-1 text-sm font-normal leading-[22px]">{formattedDate}</p>
-          <Link href={`/news/${news?.slug}`}>
-          <h3 className="line-clamp-2 font-onest text-[32px] font-medium leading-[44px] text-white">
-            {title}
-          </h3>
-          </Link>
+          <p className="mb-1 text-sm font-normal leading-[22px]">
+            {formattedDate}
+          </p>
+          <div>
+            <h3 className="line-clamp-2 font-onest text-[32px] font-medium leading-[44px] text-white">
+              {title}
+            </h3>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
